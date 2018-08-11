810f3f561886f8b9771263019d013d2ef943da1a
mwbitt <mwbitt@gmail.com>
2018-08-11 07:56:09 -0300

    add constrollers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,48 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'dashboards/index'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      get 'recommendations/index'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      get 'executions/create'
+      get 'executions/update'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      get 'series/show'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      get 'movies/show'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      get 'searches/index'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      get 'reviews/index'
+      get 'reviews/create'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      get 'favorites/index'
+      get 'favorites/create'
+      get 'favorites/destroy'
+    end
+  end
   devise_for :users
   root to: "home#index"
  end
